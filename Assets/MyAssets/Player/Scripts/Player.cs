ea691692d8c9f61bf22aa3b456bbd1ce7459fccf
macha1316 <hiromacha1116@gmail.com>
2025-03-22 17:01:19 +0900

    add: Goal処理

--- Assets/MyAssets/Player/Scripts/Player.cs
+++ Assets/MyAssets/Player/Scripts/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour, ITurnBased
         Vector3 nextPos = transform.position + transform.forward * 2.0f;
 
         // 範囲外 or ブロック → 反転
-        if (!IsValidPosition(nextPos) || IsBlockedPosition(nextPos))
+        if (!IsValidPosition(nextPos) || IsMatchingCellType(nextPos, 'B'))
         {
             transform.forward = -transform.forward;
             nextPos = transform.position + transform.forward * 2.0f;
@@ -27,10 +27,15 @@ public class Player : MonoBehaviour, ITurnBased
             {
                 isMoving = false;
 
-                // nextPos に到達した後で方向変更ギミックをチェック
-                if (IsDirectionChangeTile(transform.position))
+                // Dynamic
+                if (IsMatchingDynamicCellType(transform.position, 'Y'))
                 {
                     transform.forward = Vector3.right;
+                }
+                // 通常
+                if (IsMatchingCellType(transform.position, 'G'))
+                {
+                    Debug.Log("GGGGGGGoal");
                 }
             });
     }
@@ -46,22 +51,22 @@ public class Player : MonoBehaviour, ITurnBased
                row >= 0 && row < StageBuilder.Instance.GetGridData().GetLength(2);
     }
 
-    private bool IsBlockedPosition(Vector3 pos)
+    private bool IsMatchingCellType(Vector3 pos, char cellType)
     {
         int col = Mathf.RoundToInt(pos.x / StageBuilder.BLOCK_SIZE);
         int height = Mathf.RoundToInt(pos.y / StageBuilder.HEIGHT_OFFSET);
         int row = Mathf.RoundToInt(pos.z / StageBuilder.BLOCK_SIZE);
 
-        return StageBuilder.Instance.GetGridData()[col, height, row] == 'B';
+        return StageBuilder.Instance.GetGridData()[col, height, row] == cellType;
     }
 
-    private bool IsDirectionChangeTile(Vector3 pos)
+    private bool IsMatchingDynamicCellType(Vector3 pos, char cellType)
     {
         int col = Mathf.RoundToInt(pos.x / StageBuilder.BLOCK_SIZE);
         int height = Mathf.RoundToInt(pos.y / StageBuilder.HEIGHT_OFFSET);
         int row = Mathf.RoundToInt(pos.z / StageBuilder.BLOCK_SIZE);
 
-        return StageBuilder.Instance.GetDynamicGridData()[col, height, row] == 'Y';
+        return StageBuilder.Instance.GetDynamicGridData()[col, height, row] == cellType;
     }
 
     public void UpdateGridData()
