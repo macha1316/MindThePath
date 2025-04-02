4a1ec97feac259cbd03ab42ad30ad3c59a7b349a
macha1316 <hiromacha1116@gmail.com>
2025-04-02 23:32:56 +0900

    add: GM作成

---
+++ Assets/MyAssets/GM/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private List<Player> players;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        players = new List<Player>();
+    }
+
+    public void GetPlayer(Player newP)
+    {
+        players.Add(newP);
+    }
+
+    public bool IsComplete()
+    {
+        foreach (var player in players)
+        {
+            if (!player.GetIsComplete()) return false;
+        }
+        return true;
+    }
+}
