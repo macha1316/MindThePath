34c92304da6aac95e60f78feaae1dbc30d8132e4
macha1316 <hiromacha1116@gmail.com>
2025-04-06 23:37:01 +0900

    add: 落下処理

--- Assets/MyAssets/MoveBox/Scripts/MoveBox.cs
+++ Assets/MyAssets/MoveBox/Scripts/MoveBox.cs
@@ -27,7 +27,7 @@ public class MoveBox : MonoBehaviour, ITurnBased
 
         transform.DOMove(targetPos, moveDuration).SetEase(Ease.Linear).OnComplete(() =>
         {
-            isMoving = false;
+            FallIfNeeded();
         });
     }
 
@@ -38,6 +38,47 @@ public class MoveBox : MonoBehaviour, ITurnBased
 
     public void UpdateGridData()
     {
+        // いきなりtargetPosに合わせるの問題になる？かも
         StageBuilder.Instance.UpdateGridAtPosition(targetPos, 'M');
+    }
+
+    private void FallIfNeeded()
+    {
+        Vector3 currentPos = targetPos;
+        Vector3 down = Vector3.down * StageBuilder.HEIGHT_OFFSET;
+
+        while (true)
+        {
+            Vector3 belowPos = currentPos + down;
+            if (!StageBuilder.Instance.IsValidGridPosition(belowPos)) break;
+
+            int col = Mathf.RoundToInt(belowPos.x / StageBuilder.BLOCK_SIZE);
+            int height = Mathf.RoundToInt(belowPos.y / StageBuilder.HEIGHT_OFFSET);
+            int row = Mathf.RoundToInt(belowPos.z / StageBuilder.BLOCK_SIZE);
+
+            char[,,] grid = StageBuilder.Instance.GetGridData();
+            if (grid[col, height, row] != 'N') break;
+
+            currentPos = belowPos;
+        }
+
+        if (currentPos != targetPos)
+        {
+            StageBuilder.Instance.UpdateGridAtPosition(targetPos, 'N');
+            targetPos = currentPos;
+
+            // 何マス落ちるかを計算
+            int fallBlocks = Mathf.RoundToInt((transform.position.y - targetPos.y) / StageBuilder.HEIGHT_OFFSET);
+            float fallTime = fallBlocks * 1f;
+
+            transform.DOMove(targetPos, fallTime).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                isMoving = false;
+            });
+        }
+        else
+        {
+            isMoving = false;
+        }
     }
 }
